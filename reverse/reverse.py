f777ecba4ec6dcd269f2cafa79bb41a80e4f5b3a
Hathos_Vanox <49489547+AidanMahoney73@users.noreply.github.com>
2020-08-02 23:46:34 -0400

    add changes

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -40,3 +40,40 @@ class LinkedList:
 
     def reverse_list(self, node, prev):
         pass
+
+    # def reverse_list(self, node, prev):
+    #     if node is None:
+    #         return None
+
+    #     if node.next_node is not None:
+    #         self.reverse_list(node.next_node, node)
+    #         node.set_next(prev)
+    #     else:
+    #         node.set_next(prev)
+    #         self.head = node
+
+if __name__ == "__main__":
+    llist = LinkedList()
+    llist.add_to_head(1)
+    llist.add_to_head(2)
+    llist.add_to_head(3)
+    llist.add_to_head(4)
+    llist.add_to_head(5)
+
+    self.assertEqual(self.list.head.value, 5)
+    self.list.reverse_list(self.list.head, None)
+    self.assertEqual(self.list.head.value, 1)
+    self.assertEqual(self.list.head.get_next().value, 2)
+    self.assertEqual(self.list.head.get_next().get_next().value, 3)
+
+
+    self.list.add_to_head(1)
+    self.list.add_to_head(2)
+    self.list.add_to_head(3)
+    self.list.add_to_head(4)
+    self.list.add_to_head(5)
+    self.assertEqual(self.list.head.value, 5)
+    self.list.reverse_list(self.list.head, None)
+    self.assertEqual(self.list.head.value, 1)
+    self.assertEqual(self.list.head.get_next().value, 2)
+    self.assertEqual(self.list.head.get_next().get_next().value, 3)
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,69 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
 
     def append(self, item):
-        pass
+        #* iterate through list to decrease the history value by one
+        #* append data to list
+            #* append to the end of the list if list not full
+            #* if list full replace item with history 0
+
+        for element in self.data:
+            element[1] -= 1
+
+        if len(self.data) < self.capacity:
+            self.data.append([item, self.capacity])
+        else:
+            for element in range(len(self.data)):
+                if self.data[element][1] == 0:
+                    self.data[element] = [item, self.capacity]
 
     def get(self):
-        pass
+        return [element[0] for element in self.data]
+
+    def test(self):
+        return self.data
+
+
+# buffer = RingBuffer(3)
+
+# buffer.get()   #* should return []
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# buffer.get()   #* should return ['a', 'b', 'c']
+
+# #* 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+
+# buffer.get()   #* should return ['d', 'b', 'c']
+
+# buffer.append('e')
+# buffer.append('f')
+
+# buffer.get()   #* should return ['d', 'e', 'f']
+if __name__ == "__main__":
+    #* Test One
+    capacity = 5
+    buffer = RingBuffer(capacity)
+    assert buffer.capacity == capacity
+
+    #* Test Two
+    buffer.append('a')
+    assert buffer.get() == ['a']
+
+    #! Test Three
+    buffer.append('a')
+    buffer.append('b')
+    buffer.append('c')
+    buffer.append('d')
+    print("===========start==========")
+    print(buffer.test())
+    print(buffer.get())
+    buffer.append('e')
+    print(buffer.test())
+    print(buffer.get())
+    print("===========end============")
